e156ea8faa40e2a6ee7d7b9e89fe2c5a4da90160
Kauã Henrique Fernandez dos Santos <137641063+KauaHenriqueFS@users.noreply.github.com>
2025-08-27 09:06:49 -0300

    Exercício 1 - Feito

---
+++ Exercicio1/src/java/model/Func.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author aluno
+ */
+@Entity
+@Table(name = "func")
+@NamedQueries({
+    @NamedQuery(name = "Func.findAll", query = "SELECT f FROM Func f"),
+    @NamedQuery(name = "Func.findByCodigo", query = "SELECT f FROM Func f WHERE f.codigo = :codigo"),
+    @NamedQuery(name = "Func.findByNome", query = "SELECT f FROM Func f WHERE f.nome = :nome"),
+    @NamedQuery(name = "Func.findByPeso", query = "SELECT f FROM Func f WHERE f.peso = :peso")})
+public class Func implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "codigo")
+    private Integer codigo;
+    @Column(name = "nome")
+    private String nome;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "peso")
+    private Double peso;
+
+    public Func() {
+    }
+
+    public Func(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (codigo != null ? codigo.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Func)) {
+            return false;
+        }
+        Func other = (Func) object;
+        if ((this.codigo == null && other.codigo != null) || (this.codigo != null && !this.codigo.equals(other.codigo))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Func[ codigo=" + codigo + " ]";
+    }
+    
+}
